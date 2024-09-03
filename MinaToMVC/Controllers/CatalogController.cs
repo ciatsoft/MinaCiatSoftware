1d71fd3f8b60ac37e377c8a5f4e66a2dcd277bd8
LAPTOP-TJLEOC8Q\JOSE <inocar22.15@gmail.com>
2024-09-02 23:31:12 -0600

    Se agrega acceso a datos en MVC para catalogo unidad de medida

--- MinaToMVC/Controllers/CatalogController.cs
+++ MinaToMVC/Controllers/CatalogController.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace MinaToMVC.Controllers
 {
-    public class CatalogController : Controller
+    public class CatalogController : BaseController
     {
         // GET: Catalog
         public ActionResult Index()
@@ -14,9 +16,18 @@ namespace MinaToMVC.Controllers
             return View();
         }
 
+
+
+        #region Unidad de Medida
         public ActionResult UnidadMedida()
         {
             return View();
         }
+        public async Task<string> GetAllUnidadmedida()
+        {
+            var result = await httpClientConnection.GetAllUnidadMedida();
+            return JsonConvert.SerializeObject(result);
+        }
+        #endregion
     }
 }
