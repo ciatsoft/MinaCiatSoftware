27e3a7acea26985eb8cadd5a2c0a7bdb9a930c89
ibartolo <bartolocastro@gmail.com>
2024-09-25 00:43:26 -0600

    Se arreglan Clases para merge del despapaye

---
+++ MinaTolEntidades/DtoCatalogos/DtoUbicacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinaTolEntidades.DtoCatalogos
+{
+    public class DtoUbicacion : BaseObject
+    {
+        public string NombreUbicacion { get; set; }
+        public string DescripcionUbicacion { get; set; }
+
+    }
+}
