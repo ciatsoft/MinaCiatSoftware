f80f4ce35049a9a109c79583f929cf16a91e5884
LAPTOP-TJLEOC8Q\JOSE <inocar22.15@gmail.com>
2024-10-02 21:08:56 -0600

    Se suben cambios para agregar la ventana de usuario

---
+++ MinaToMVC/Controllers/UsuarioController.cs
@@ -0,0 +1,49 @@
+﻿using MinaTolEntidades.Security;
+using MinaToMVC.DAL;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinaToMVC.Controllers
+{
+    public class UsuarioController : BaseController
+    {
+     
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #region Usuario
+        public async Task<ActionResult> AltaEdicion(long id = 0)
+        {
+            var usuario = new Usuario();
+            if (id != 0)
+            {
+                var result = await httpClientConnection.GetUsuarioById(id);
+                usuario = JsonConvert.DeserializeObject<Usuario>(result.Response.ToString());
+            }
+            return View(usuario);
+        }
+        public async Task<string> GetAllUsuario()
+        {
+            var token = Helpers.SessionHelper.GetSessionUser();
+            var result = await httpClientConnection.GetAllUsuario(token.Token.access_token);
+            return Newtonsoft.Json.JsonConvert.SerializeObject(result);
+        }
+
+        public string SaveOrUpdateUsuario(Usuario u)
+        {
+            var result = httpClientConnection.SaveOrUpdateUsuario(u);
+            return JsonConvert.SerializeObject(result);
+        }
+
+
+        #endregion
+
+    }
+}
