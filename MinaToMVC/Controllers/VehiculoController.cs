c34107d1bfddd60bafa3b8c2f6226eae33e55b01
ibartolo <bartolocastro@gmail.com>
2024-10-28 10:35:54 -0600

    Se agrega funcionalidad para administrar los vehiculos

---
+++ MinaToMVC/Controllers/VehiculoController.cs
@@ -0,0 +1,39 @@
+﻿using MinaTolEntidades.DtoClientes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinaToMVC.Controllers
+{
+    public class VehiculoController : BaseController
+    {
+        public async Task<ActionResult>  Index(long id = 0)
+        {
+            Vehiculo vehiculo;
+
+            if (id != 0)
+            {
+                vehiculo = new Vehiculo();
+            }
+            else
+            {
+                vehiculo = new Vehiculo();
+            }
+
+            var tiposVehiculoResponse = await httpClientConnection.GetAllTipoVehiculo();
+            var tiposVehiculo = JsonConvert.DeserializeObject<List<TipoVehiculo>>(tiposVehiculoResponse.Response.ToString());
+
+            var tiposVehiculoList = MappingPropertiToDropDownList(tiposVehiculo, "Id", "Nombre");
+
+            ViewBag.TipoVehiculos = tiposVehiculoList; ;
+
+
+            return View(vehiculo);
+        }
+
+    }
+}
