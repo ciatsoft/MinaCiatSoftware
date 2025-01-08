6fcad3eaf30ac54120dd7f23bd13a443f0b0d6b3
ibartolo <bartolocastro@gmail.com>
2025-01-07 23:50:37 -0600

    Se modifica objeto para configuración

--- MinaTolEntidades/DtoClientes/ClienteTipoMaterial.cs
+++ MinaTolEntidades/DtoClientes/ClienteTipoMaterial.cs
@@ -16,5 +16,19 @@ namespace MinaTolEntidades.DtoClientes
 
         public Cliente Cliente { get; set; }
         public DtoTipoMaterialUbicacion TipoMaterial { get; set; }
+        public decimal P_Mta_M3 { get; set; }
+        public decimal P_Flete_M3 { get; set; }
+        public decimal Precio_M3 { get; set; }
+        public decimal KM_Cargado { get; set; }
+        public decimal KM_Basico { get; set; }
+        public decimal Total_KM_Recorridos { get; set; }
+        public decimal Carga_Disel { get; set; }
+        public decimal Total_Diesel_Precio_XLT { get; set; }
+        public decimal Casetas { get; set; }
+        public decimal Mano_De_Obra { get; set; }
+        public decimal Material_Viajes_De_30M3 { get; set; }
+        public decimal Total_Gastos { get; set; }
+        public decimal Subtotal_Ingreso_Viajes_M3 { get; set; }
+
     }
 }
