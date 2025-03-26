359196a75b822594313adde91e148cd56a18e504
FranciscodSW <frosasg002@alumno.uaemex.mx>
2025-03-26 14:26:53 -0600

    Primera subidad de codigo compile el proyecto y validaciones en html. creacion de entidade para la tabla PV_Material, se creeo el constructor y se realizaron modificacione a la api

---
+++ MinaTolEntidades/DtoVentas/PV_Material.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinaTolEntidades.DtoSucursales
+{
+    public class PV_Material : BaseObject
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
