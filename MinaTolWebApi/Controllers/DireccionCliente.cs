3363a6390cdd05fcc81ce32faeba5ca4b881449b
FranciscodSW <frosasg002@alumno.uaemex.mx>
2025-04-04 13:56:21 -0600

    Seacraron los controladores de Dirreccion cliente, se trabajo en el apartado de web api, en el apartado de entidades y en el apartada del DbWraper

---
+++ MinaTolWebApi/Controllers/DireccionCliente.cs
@@ -0,0 +1,52 @@
+﻿using MinaTolEntidades;
+using MinaTolEntidades.DtoCatalogos;
+using MinaTolEntidades.DtoClientes;
+using MinaTolWebApi.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MinaTolWebApi.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/DireccionCliente")]
+    public class DireccionClienteController : ApiController
+    {
+        private DbWrapper wrapper { get; set; }
+        public DireccionClienteController()
+        {
+            wrapper = new DbWrapper();
+        }
+        
+        [Route("List"), HttpGet]
+        public ModelResponse GetAllDireccionCliente()
+        {
+            var result = wrapper.GetAllDireccionCliente();
+            return result;
+        }
+
+        [HttpGet, Route("{id:long}")]
+        public ModelResponse GetDireccionClienteById(int id)
+        {
+            var result = wrapper.GetDireccionClienteById(id);
+            return result;
+        }
+
+        [HttpPost, Route("")]
+        public ModelResponse SaveOrUpdateDireccionCliente(DireccionCliente at)
+        {
+            var result = wrapper.SaveOrUpdateDireccionCliente(at);
+            return result;
+        }
+        [HttpDelete, Route("{id:long}")]
+        public ModelResponse DeleteDireccionCliente(int id)
+        {
+            var result = wrapper.DeleteDireccionCliente(id);
+            return result;
+        }
+    }
+}
---
+++ MinaTolWebApi/DAL/DbWrapper.DireccionCliente.cs
@@ -0,0 +1,120 @@
+﻿using MinaTolEntidades.Security;
+using MinaTolEntidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using MinaTolEntidades.DtoCatalogos;
+
+namespace MinaTolWebApi.DAL
+{
+    public partial class DbWrapper
+    {
+        public ModelResponse GetAllDireccionCliente()
+        {
+            var modelResponse = new ModelResponse();
+            var parameters = new List<SqlParameter>();
+
+            try
+            {
+                var result = GetObjects($"GetAll", CommandType.Text,parameters,
+                    new Func<IDataReader, DtoAreaTrabajo>((reader) =>
+                    {
+                        var r = FillEntity<DtoAreaTrabajo>(reader);
+
+                        return r;
+                    }));
+
+                modelResponse.Response = result;
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Enum = Enumeration.ErrorNoControlado;
+                modelResponse.Message = ex.Message;
+            }
+
+            return modelResponse;
+        }
+        public ModelResponse SaveOrUpdateAreaTrabajo(DtoAreaTrabajo at)
+        {
+            var response = new ModelResponse();
+            try
+            {
+                response.IsSuccess = true;
+                var parameters = GenerateSQLParameters(at);
+                var areatrabajoId = ExecuteScalar($"SaveOrUpdateAreaTrabajo",System.Data.CommandType.StoredProcedure, parameters);
+                at.Id = Convert.ToInt64(areatrabajoId);
+
+                response.Response = at;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+                response.Enum = Enumeration.ErrorNoControlado;
+            }
+            return response;
+        }
+        public ModelResponse GetAreaTrabajoById(int id)
+        {
+            var response = new ModelResponse();
+            try
+            {
+                response.IsSuccess = true;
+                var parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter()
+                {
+                    Value = id,
+                    IsNullable = true,
+                    ParameterName = "@Id",
+                    SqlDbType = System.Data.SqlDbType.Int
+                });
+
+                var result = GetObject("GetAreaTrabajoById", System.Data.CommandType.StoredProcedure,
+                    parameters, new Func<System.Data.IDataReader, DtoAreaTrabajo>((reader) =>
+                    {
+                        var r = FillEntity<DtoAreaTrabajo>(reader);
+                        return r;
+                    }));
+                response.Response = result;
+
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+                response.Enum = Enumeration.ErrorNoControlado;
+            }
+            return response;
+        }
+        public ModelResponse DeleteAreaTrabajo(int id)
+        {
+            var response = new ModelResponse();
+            try
+            {
+                response.IsSuccess = true;
+                var parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter()
+                {
+                    Value = id,
+                    IsNullable = true,
+                    ParameterName = "@Id",
+                    SqlDbType = System.Data.SqlDbType.Int
+                });
+
+                var result = ExecuteNonQuery("DeleteAreaTrabajo", System.Data.CommandType.StoredProcedure, parameters);
+
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+                response.Enum = Enumeration.ErrorNoControlado;
+            }
+            return response;
+        }
+    }
+}
