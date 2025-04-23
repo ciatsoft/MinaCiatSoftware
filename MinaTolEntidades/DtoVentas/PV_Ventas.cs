65d77eeb8de013c69f187e5f1021e62bad705dfa
Ivan <bartolocastro@gmail.com>
2025-04-23 01:22:29 -0600

    Se agrega controlador para venta publico en general

---
+++ MinaTolEntidades/DtoVentas/PV_Ventas.cs
@@ -0,0 +1,25 @@
+﻿using MinaTolEntidades.DtoCatalogos;
+using MinaTolEntidades.DtoSucursales;
+using MinaTolEntidades.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinaTolEntidades.DtoVentas
+{
+    public class PV_Ventas : BaseObject
+    {
+        public string Folio { get; set; }
+        public DtoUbicacion Ubicacion { get; set; }
+        public DtoTipoMaterialUbicacion TipoMaterial { get; set; }
+        public string FormaDePago { get; set; }
+        public string Transporte { get; set; }
+        public string Placa { get; set; }
+        public string Cantidad { get; set; }
+        public UnidadMedida UnidadMedida { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
