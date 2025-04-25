543ae4f693f40df19dfe327ec457517acaf5f2f3
FranciscodSW <frosasg002@alumno.uaemex.mx>
2025-04-25 11:49:56 -0600

    Espacios para dar mejor formato

--- MinaTolEntidades/DtoIngresosRfid/IngresosRfid.cs
+++ MinaTolEntidades/DtoIngresosRfid/IngresosRfid.cs
@@ -9,11 +9,8 @@ namespace MinaTolEntidades.DtoIngresosRfid
     public class IngresosRfid : BaseObject 
 
     {
-        
         public string RfidId { get; set; }
         public DateTime Fecha { get; set; }
         public string Accesos { get; set; }
-       
-
     }
 }
