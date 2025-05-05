d4d5f3c505428c9549be8408b94051088618460e
JUAN MANUEL RUARO ZEPEDA <al222210649@gmail.com>
2025-05-05 10:26:57 -0600

    commit desde visual

--- MinaToMVC/DAL/HttpClientConnection.RFID.cs
+++ MinaToMVC/DAL/HttpClientConnection.RFID.cs
@@ -1,18 +1,68 @@
-﻿using MinaTolEntidades;
-using MinaTolEntidades.Dto_Rfid;
-using MinaToMVC.Controllers;
-using MinaToMVC.Helpers;
+﻿using MinaTolEntidades.DtoCatalogos;
+using MinaTolEntidades;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
-using System.Web;
+using MinaTolEntidades.Dto_Rfid;
 
 namespace MinaToMVC.DAL
 {
-    public class HttpClientConnection
+    public partial class HttpClientConnection
     {
+
+        public async Task<ModelResponse> SaveOrUpdateRfid(Rfid u)
+        {
+            MappingColumSecurity(u);
+            var result = await RequestAsync<object>("api/Rfid", HttpMethod.Post, u,
+            new Func<string, string>((responseString) =>
+            {
+                return responseString;
+            }), token.Token.access_token);
+            var modelResponse = JsonConvert.DeserializeObject<ModelResponse>(result.ToString());
+
+            return modelResponse;
+
+        }
+        public async Task<ModelResponse> GetAllRfid()
+        {
+            var result = await RequestAsync<object>("api/Rfid", HttpMethod.Get, null,
+            new Func<string, string>((responseString) =>
+            {
+                return responseString;
+            }), token.Token.access_token);
+            var modelResponse = JsonConvert.DeserializeObject<ModelResponse>(result.ToString());
+
+            return modelResponse;
+
+        }
+        public async Task<ModelResponse> GetRfidById(long id)
+        {
+            var result = await RequestAsync<object>($"api/Rfid/{id}", HttpMethod.Get, null,
+            new Func<string, string>((responseString) =>
+            {
+                return responseString;
+            }), token.Token.access_token);
+            var modelResponse = JsonConvert.DeserializeObject<ModelResponse>(result.ToString());
+
+            return modelResponse;
+
+        }
+        public async Task<ModelResponse> DeleteRfid(long id)
+        {
+            var result = await RequestAsync<object>($"api/Rfid/{id}", HttpMethod.Delete, null,
+            new Func<string, string>((responseString) =>
+            {
+                return responseString;
+            }), token.Token.access_token);
+            var modelResponse = JsonConvert.DeserializeObject<ModelResponse>(result.ToString());
+
+            return modelResponse;
+
+        }
     }
 }
