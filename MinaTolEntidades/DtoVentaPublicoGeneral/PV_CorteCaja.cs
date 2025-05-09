abbfebebe9ba82d0ab8b7dbd167191daee598c27
FranciscodSW <frosasg002@alumno.uaemex.mx>
2025-05-09 11:25:45 -0600

    Creacion de entidades, controllers, DbWrapper de corte de caja se implementa la vista, se crearon los procedimientos basicos

---
+++ MinaTolEntidades/DtoVentaPublicoGeneral/PV_CorteCaja.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinaTolEntidades.DtoVentaPublicoGeneral
+{
+    public class PV_CorteCaja : BaseObject
+    {
+        public double MontoTotal { get; set; }
+        public double Ingreso { get; set; }
+        public double Egreso { get; set; }
+        public string Comentarioa { get; set; }
+        public int c500 {get; set;}
+        public int c200 { get; set; }
+        public int c100 { get; set; }
+        public int c50 { get; set; }
+        public int c10 { get; set; }
+        public int c5 { get; set; }
+        public int c2 { get; set; }
+        public int c1 { get; set; }
+        public int c50c{ get; set; }
+    }
+}
---
+++ MinaTolWebApi/DAL/DbWrapper.PV_CorteCaja.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using MinaTolEntidades.DtoCatalogos;
+using MinaTolEntidades;
+using MinaTolEntidades.DtoVentaPublicoGeneral;
+
+namespace MinaTolWebApi.DAL
+{
+    public partial class DbWrapper
+    {
+        public ModelResponse SaveOrUpdatePV_CorteCaja(PV_CorteCaja tv)
+        {
+            var response = new ModelResponse();
+            try
+            {
+                response.IsSuccess = true;
+                var parameters = GenerateSQLParameters(tv);
+                var result = GetObject("SaveOrUpdatePV_CorteCaja", System.Data.CommandType.StoredProcedure,
+                    parameters, new Func<System.Data.IDataReader, PV_CorteCaja>((reader) =>
+                    {
+                        var r = FillEntity<PV_CorteCaja>(reader);
+                        return r;
+                    }));
+                response.Response = result;
+
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+                response.Enum = Enumeration.ErrorNoControlado;
+
+            }
+            return response;
+        }
+        public ModelResponse GetAllPV_CorteCaja()
+        {
+            var response = new ModelResponse();
+            try
+            {
+                response.IsSuccess = true;
+                var parameters = new List<SqlParameter>();
+                var result = GetObjects("GetAllPV_CorteCaja", System.Data.CommandType.StoredProcedure,
+                    parameters, new Func<System.Data.IDataReader, PV_CorteCaja>((reader) =>
+                    {
+                        var r = FillEntity<PV_CorteCaja>(reader);
+                        return r;
+                    }));
+                response.Response = result;
+
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+                response.Enum = Enumeration.ErrorNoControlado;
+            }
+            return response;
+        }
+
+        public ModelResponse GetPV_CorteCajaById(int id)
+        {
+            var response = new ModelResponse();
+            try
+            {
+                response.IsSuccess = true;
+                var parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter()
+                {
+                    Value = id,
+                    IsNullable = true,
+                    ParameterName = "@Id",
+                    SqlDbType = System.Data.SqlDbType.Int
+                });
+
+                var result = GetObject("GetPV_CorteCajaById", System.Data.CommandType.StoredProcedure,
+                    parameters, new Func<System.Data.IDataReader, PV_CorteCaja>((reader) =>
+                    {
+                        var r = FillEntity<PV_CorteCaja>(reader);
+                        return r;
+                    }));
+                response.Response = result;
+
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+                response.Enum = Enumeration.ErrorNoControlado;
+            }
+            return response;
+        }
+        public ModelResponse DeletePV_CorteCaja(int id)
+        {
+            var response = new ModelResponse();
+            try
+            {
+                response.IsSuccess = true;
+                var parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter()
+                {
+                    Value = id,
+                    IsNullable = true,
+                    ParameterName = "@Id",
+                    SqlDbType = System.Data.SqlDbType.Int
+                });
+
+                var result = ExecuteNonQuery("DeletePV_CorteCaja", System.Data.CommandType.StoredProcedure, parameters);
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+                response.Enum = Enumeration.ErrorNoControlado;
+            }
+            return response;
+        }
+    }
+}
