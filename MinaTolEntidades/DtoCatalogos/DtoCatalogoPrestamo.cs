e47c0cf2d6c8393b621be4d08515b729a035cdfa
AlanVD49 <al222210758@gmail.com>
2025-06-02 16:26:57 -0600

    Se trabajo el Backend para prestamos y tipo de gastos (CRUD)

---
+++ MinaTolEntidades/DtoCatalogos/DtoCatalogoPrestamo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinaTolEntidades.DtoCatalogos
+{
+    public class DtoCatalogoPrestamo : BaseObject
+    {
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal Monto { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string UsuarioName { get; set; }
+
+
+    }
+}
---
+++ MinaTolEntidades/DtoCatalogos/DtoTipoGasto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinaTolEntidades.DtoCatalogos
+{
+    public class DtoTipoGasto : BaseObject
+    {
+        public long Id_Prestamos { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal Monto { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string UsuarioName { get; set; }
+    }
+}
