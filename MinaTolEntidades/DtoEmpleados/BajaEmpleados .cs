eb1de185ca0dac5136ac2319bce5e0a95bdb399d
Yojan338 <contacto.yy.studio@gmail.com>
2025-09-29 12:18:55 -0600

    Back de bajas

---
+++ MinaTolEntidades/DtoEmpleados/BajaEmpleados .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinaTolEntidades.DtoEmpleados
+{
+    public class DtoBajasEmpleado : BaseObject
+    {
+        public long IdTrabajador { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Comentario { get; set; }
+    }
+}
