127e4cc0b1d443764c90b20e0496364e19ca1717
DESKTOP-P49T3M6\JuanPC <al222210649@gmail.com>
2026-01-09 16:46:55 -0600

    Comienzo de ultima clase de Catalogos

---
+++ Catalogs.Domain/MaterialTypeLocation.cs
@@ -0,0 +1,37 @@
+﻿using Common.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Branches.Domain;
+using System.Text.Json.Serialization;
+
+namespace Catalogs.Domain
+{
+    public class MaterialTypeLocation : Entity<long>
+    {
+        private UnitMeasurement _unitMeasurement;
+        private long? _unitMeasurementId;
+
+        [JsonPropertyName("UbicacionId")]
+        public long locationId { get; private set; }
+        [JsonPropertyName("MaterialId")]
+        public long materialId { get; private set; }
+        [JsonPropertyName("NombreTipoMaterial")]
+        public string nameTypeMaterial { get; private set; }
+        [JsonPropertyName("DescripcionTipoMaterial")]
+        public string descriptionTypeMaterial { get; private set; }
+
+        [JsonPropertyName("UnidadMedidaID")]
+        public long? UnitMeasureID => _unitMeasurementId ?? _unitMeasurement?.Id;
+
+        [JsonIgnore]
+        public UnitMeasurement? UnitMeasurement => _unitMeasurement;
+
+        //[JsonConstructor]
+        //public MaterialTypeLocation(
+            
+        //    )
+    }
+}
