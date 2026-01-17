9ffc954545c8cf2c5e04dcdc3b1577cc14dc6f12
DESKTOP-P49T3M6\JuanPC <al222210649@gmail.com>
2026-01-17 15:14:35 -0600

    Actualizacion de vehiculos para carga MINA Etapa 2

---
+++ MinaToMVC/Controllers/VehiculoCargaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinaToMVC.Controllers
+{
+    public class VehiculoCargaController : Controller
+    {
+        #region Views
+
+        #region VehiculoCarga
+        public ActionResult VehiculoCarga()
+        {
+            return View();
+        }
+        #endregion
+
+        #endregion
+
+        #region PartialViews
+
+        #endregion
+
+        #region AccessData
+
+        #region VehiculoCarga
+
+        #endregion
+
+        #endregion
+    }
+}
