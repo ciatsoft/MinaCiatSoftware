7fe013fdce61c45206cd7feab791eb008241b212
DESKTOP-P49T3M6\JuanPC <al222210649@gmail.com>
2026-01-19 16:41:01 -0600

    Actualizacion de Controlador MVC

--- MinaToMVC/Controllers/VehiculoCargaController.cs
+++ MinaToMVC/Controllers/VehiculoCargaController.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using MinaTolEntidades;
+using MinaTolEntidades.DtoCatalogos;
+using MinaTolEntidades.DtoClientes;
+using MinaTolEntidades.DtoEmpleados;
+using MinaTolEntidades.DtoSeguridad;
+using MinaTolEntidades.DtoSucursales;
+using MinaTolEntidades.DtoViajes;
+using MinaTolEntidades.Security;
+using MinaTolEntidades.VehiculoCarga;
+using MinaToMVC.DAL;
+using MinaToMVC.Helpers;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Messaging;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using static MinaToMVC.Controllers.Filters.FiltersHelper;
 
 namespace MinaToMVC.Controllers
 {
-    public class VehiculoCargaController : Controller
+    public class VehiculoCargaController : BaseController
     {
         #region Views
 
@@ -26,7 +47,26 @@ namespace MinaToMVC.Controllers
         #region AccessData
 
         #region VehiculoCarga
-
+        public async Task<string> GetAllVehiculoCarga()
+        {
+            var result = await httpClientConnection.GetAllVehiculoCarga();
+            return Newtonsoft.Json.JsonConvert.SerializeObject(result);
+        }
+        public async Task<string> GetVehiculoCargaById(long id)
+        {
+            var result = await httpClientConnection.GetVehiculoCargaById(id);
+            return Newtonsoft.Json.JsonConvert.SerializeObject(result);
+        }
+        public string SaveOrUpdateVehiculoCarga(VehiculoCarga vc)
+        {
+            var result = httpClientConnection.SaveOrUpdateVehiculoCarga(vc);
+            return JsonConvert.SerializeObject(result);
+        }
+        public async Task<ActionResult> DeleteVehiculoCarga(long id)
+        {
+            var result = httpClientConnection.DeleteVehiculoCarga(id);
+            return Redirect("VehiculoCarga");
+        }
         #endregion
 
         #endregion
