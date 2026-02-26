a96bf4b063e54e69bf8d19a091c9dd4780940b9a
DESKTOP-P49T3M6\JuanPC <al222210649@gmail.com>
2026-02-26 17:43:37 -0600

    Avance de Taller

---
+++ MinaTolEntidades/DtoTaller/ReparacionVehiculos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinaTolEntidades.DtoTaller
+{
+    public class ReparacionVehiculos : BaseObject
+    {
+        public long IdVehiculo { get; set; }
+        public int TipoVehiculo { get; set; }
+        public long IdEmpleado { get; set; }
+        public string TipoServicio { get; set; }
+        public string Recibido { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
